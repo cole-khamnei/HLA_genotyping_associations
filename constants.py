6fea3ea38694040b817dc2879d06949b9b3fb04b
Cole Khamnei <cole.khamnei@gmail.com>
2022-05-02 21:33:04 -0400

    update

--- constants.py
+++ constants.py
@@ -1,8 +1,20 @@
+import platform
+
 ###############################################################################################################
 ### Constants ###
 ###############################################################################################################
 
-UK_BIOBANK_DATA_PATH = "C://Users//Cole//Documents//_drive//columbia_education//research//rabadan_lab//data//uk_biobank"
+if platform.system() == 'Darwin':
+    DATA_PATH = "/Users/cole/Documents/_research/rabadan_lab/data"
+    dir_divider = "/"
+elif platform.system() == 'Windows':
+    DATA_PATH = "C://Users//Cole//Documents//_drive//columbia_education//research//rabadan_lab//data"
+    dir_divider = "//"
+
+else:
+    raise NotImplementedError("Unsupported OS")        
+
+UK_BIOBANK_DATA_PATH = DATA_PATH + f"{dir_divider}uk_biobank"
 
 ###############################################################################################################
 ### End ###
