cc5ddae8b983802502c5594cb15b8158bcf09014
Cole Khamnei <cole.khamnei@gmail.com>
2022-06-27 10:55:01 -0400

    routine

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import time
 import sys
 from gc import get_referents
+import six
 
 from typing import Any, List, Optional, Iterable
 from types import ModuleType, FunctionType
@@ -261,6 +262,32 @@ def sns_wrapper(function, data, x, hue: str = None, ax=None, label: str = None, 
         ax.legend()
 
     return ax
+
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, fig=None, **kwargs):
+    """ stolen from stack overflow """
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+
+    return fig, ax
 
 
 ########################################################################################################################
